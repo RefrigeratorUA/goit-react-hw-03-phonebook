89e6e447f3f78294bc363735d943b421481acf22
Dmytro Shalenyi <shalenyi.dm@gmail.com>
2021-01-20 13:42:09 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook to HW3</h1>
+      </>
+    );
+  }
+}
